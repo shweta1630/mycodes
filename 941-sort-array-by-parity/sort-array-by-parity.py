61fd9c89314162c2803406ad1c8c5cba6b23ea67
Shweta Dayama <24dayamas@rbunagpur.in>
2026-05-13 07:39:58 -0700

    Time: 10 ms (6.03%) | Memory: 12.7 MB (99.64%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.py
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -6,10 +6,10 @@ class Solution(object):
             if nums[left] % 2 > nums[right] % 2:
                 nums[left], nums[right] = nums[right], nums[left]
 
-            if nums[left] % 2 == 0:
+            if nums[left] &1 == 0:
                 left += 1
 
-            if nums[right] % 2 == 1:
+            if nums[right] &1 == 1:
                 right -= 1
 
         return nums
