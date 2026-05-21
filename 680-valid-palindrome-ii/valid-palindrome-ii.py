5b589cb05317cf327d57792524f1132053ef3f9a
Shweta Dayama <24dayamas@rbunagpur.in>
2026-05-20 23:01:15 -0700

    Time: 79 ms (66.58%) | Memory: 12.5 MB (71.84%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -12,9 +12,7 @@ class Solution(object):
                 left += 1
                 right -= 1
             return True
-
         i, j = 0, len(s) - 1
-
         while i < j:
             if s[i] != s[j]:
                 return is_palindrome(i + 1, j) or is_palindrome(i, j - 1)
