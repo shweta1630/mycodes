275f316c4c69d34ec51a94c3fa0865cfdc4e2fde
Shweta Dayama <24dayamas@rbunagpur.in>
2026-05-22 04:18:22 -0700

    Time: 0 ms (100.00%) | Memory: 12.4 MB (53.90%) - LeetSync

--- 258-add-digits/add-digits.py
+++ 258-add-digits/add-digits.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def addDigits(self, num):
-        while num >= 10:
+        while num >9:
             total = 0
             for ch in str(num):
                 total += int(ch)
