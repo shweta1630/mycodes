dcae96f906b35a400d2c568313baed4a0e1d547e
Shweta Dayama <24dayamas@rbunagpur.in>
2026-05-23 23:51:43 -0700

    Time: 33 ms (88.15%) | Memory: 19 MB (75.23%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        min_price=prices[0]
+        max_profit=0
+        for i in prices:
+            if i<min_price:
+                min_price=i
+            profit=i-min_price
+            if profit>max_profit:
+                max_profit=profit
+        return max_profit
+        
