4ae68bb7366a212e9b9d9423441689c772455da5
Shweta Dayama <24dayamas@rbunagpur.in>
2026-07-31 11:03:34 -0700

    Time: 477 ms (5.23%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -6,6 +6,7 @@ select m.employee_id,
     round(avg(e.age))as average_age
 from employees m
 join employees e
-on e.reports_to=m.employee_id
+on e.reports_to is not null
+and e.reports_to=m.employee_id
 group by m.employee_id,m.name
 order by m.employee_id;
