576f2f5325fa1e0c5fe4db427bea8b63829dc84b
Shweta Dayama <24dayamas@rbunagpur.in>
2026-07-31 11:02:29 -0700

    Time: 541 ms (5.04%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,11 @@
+-- Write your PostgreSQL query statement below
+
+select m.employee_id,
+    m.name,
+    count(e.employee_id) as reports_count,
+    round(avg(e.age))as average_age
+from employees m
+join employees e
+on e.reports_to=m.employee_id
+group by m.employee_id,m.name
+order by m.employee_id;
