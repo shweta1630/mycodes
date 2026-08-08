c96cf461314cb35372e2f266b2b2394950b7033b
Shweta Dayama <24dayamas@rbunagpur.in>
2026-08-08 10:04:58 -0700

    Time: 13 ms (22.12%) | Memory: 14 MB (51.85%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,13 +1,9 @@
 class Solution(object):
     def singleNumber(self, nums):
         nums.sort()
-
         i = 0
-
         while i < len(nums) - 1:
             if nums[i] != nums[i + 1]:
                 return nums[i]
-
             i += 2
-
         return nums[-1]
