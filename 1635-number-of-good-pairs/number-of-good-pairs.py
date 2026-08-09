3d51bd2074837465b29dda1064b1887e07a10e79
Shweta Dayama <24dayamas@rbunagpur.in>
2026-08-09 10:54:54 -0700

    Time: 0 ms (100.00%) | Memory: 12.2 MB (99.27%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.py
+++ 1635-number-of-good-pairs/number-of-good-pairs.py
@@ -7,6 +7,6 @@ class Solution(object):
         count=0
         for i in range(len(nums)-1):
             for j in range(i+1,len(nums)):
-                if nums[i]==nums[j] and i<j:
+                if nums[i]==nums[j]:
                     count+=1
         return count 
